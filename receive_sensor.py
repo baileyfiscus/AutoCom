785591ccfab6601c71a80fa74e1d96c005bc1205
Bailey <bfiscus@stevens.edu>
2018-04-11 19:46:48 +0000

    Functioning receive sensor code

--- receive_sensor.py
+++ receive_sensor.py
@@ -10,29 +10,33 @@ UDP_PORT = 5005
 sock = socket.socket(socket.AF_INET, # Internet
 socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
+GREEN = 20
+YELLOW = 21
+RED = 16
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     print "received message:", data
     data_int = int(data)
-    if data <= 60:
-        GPIO.setmode(GPIO.BCM)
-        LED = 16
-        GPIO.setup(LED, GPIO.OUT)
-        GPIO.output(LED,True)
-        time.sleep(5)
-        GPIO.output(LED,False)
-    if (60 < data <= 90):
-        GPIO.setmode(GPIO.BCM)
-        LED = 21
-        GPIO.setup(LED, GPIO.OUT)
-        GPIO.output(LED,True)
-        time.sleep(5)
-        GPIO.output(LED,False)
-    if (90 < data <= 120):
-        GPIO.setmode(GPIO.BCM)
-        LED = 20
-        GPIO.setup(LED, GPIO.OUT)
-        GPIO.output(LED,True)
-        time.sleep(5)
-        GPIO.output(LED,False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(RED, GPIO.OUT)
+    GPIO.setup(YELLOW, GPIO.OUT)
+    GPIO.setup(GREEN, GPIO.OUT)
+    if data_int <= 60:
+        print "Red"
+        GPIO.output(GREEN,False)
+        GPIO.output(YELLOW,False)
+        GPIO.output(RED,True)
+        #time.sleep(1)
+    if (60 < data_int <= 90):
+        print "Yellow"
+        GPIO.output(RED,False)
+        GPIO.output(GREEN,False)
+        GPIO.output(YELLOW,True)
+        #time.sleep(1)
+    if (90 < data_int <= 120):
+        print "Green"
+        GPIO.output(RED,False)
+        GPIO.output(YELLOW,False)
+        GPIO.output(GREEN,True)
+        #time.sleep(1)
