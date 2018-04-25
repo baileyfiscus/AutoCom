3935c3d89b26f611c73776ea5615471fb05c26ed
Bailey <bfiscus@stevens.edu>
2018-04-25 19:30:17 +0000

    Non-blocking transceiver code with Lidar

--- transceiver.py
+++ transceiver.py
@@ -1,7 +1,5 @@
-#Code to receive data over the socket
+# Author: Bailey Fiscus
 
-import threading
-from threading import Thread
 import socket
 import RPi.GPIO as GPIO
 import time
@@ -11,14 +9,17 @@ ser1 = serial.Serial('/dev/ttyS0',115200,timeout = 3)
 LIDAR = ""
 GPS = ""
 MESSAGE = ""
-UDP_IP_SEND = "155.246.202.120" # This is the IP address of the other pi
-UDP_PORT_SEND = 5005 # This has to match the UDP_PORT_RECEIVE of the other pi
+UDP_IP_SEND = "155.246.201.102" # This is the IP address of the other pi
+UDP_PORT_SEND = 5006 # This has to match the UDP_PORT_RECEIVE of the other pi
 
-UDP_IP_RECEIVE = "155.246.201.234" # This is the IP address of this pi
-UDP_PORT_RECEIVE = 5006
+UDP_IP_RECEIVE = "155.246.202.92" # This is the IP address of this pi
+UDP_PORT_RECEIVE = 5005
 sock = socket.socket(socket.AF_INET, # Internet
 socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP_RECEIVE, UDP_PORT_RECEIVE))
+sock.setblocking(0)
+
+errorVar = 0 # used to fix issue with indentation after try/ except in receive()
 GREEN = 20
 YELLOW = 21
 RED = 16
@@ -46,7 +47,7 @@ def send():
         sock.sendto(MESSAGE, (UDP_IP_SEND, UDP_PORT_SEND))
 
 def receive():
-    while True:
+    try:
         data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
         print "received message:", data
         data_int = int(data)
@@ -77,7 +78,10 @@ def receive():
             GPIO.output(RED,False)
             GPIO.output(YELLOW,False)
             GPIO.output(GREEN,False)
+    except:
+        errorVar = 0
 
 if __name__ == '__main__':
-    Thread(target = send).start()
-    Thread(target = receive).start()
+    while True:
+        send()
+        receive()
