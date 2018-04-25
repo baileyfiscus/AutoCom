1a44e333f839c6492df29fc2a94e9fd284154f2d
Bailey <bfiscus@stevens.edu>
2018-04-25 18:53:27 +0000

    Functioning code

--- transceiver_test.py
+++ transceiver_test.py
@@ -5,35 +5,33 @@ import RPi.GPIO as GPIO
 import time
 
 MESSAGE = "80085"
-UDP_IP_SEND = "155.246.202.88" # This is the IP address of the other pi
+UDP_IP_SEND = "155.246.201.102" # This is the IP address of the other pi
 UDP_PORT_SEND = 5006 # This has to match the UDP_PORT_RECEIVE of the other pi
 
-UDP_IP_RECEIVE = "155.246.201.234" # This is the IP address of this pi
+UDP_IP_RECEIVE = "155.246.202.92" # This is the IP address of this pi
 UDP_PORT_RECEIVE = 5005
 sock = socket.socket(socket.AF_INET, # Internet
 socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP_RECEIVE, UDP_PORT_RECEIVE))
+sock.setblocking(0)
+stupidVar = 0
 
 def send():
-    f.write("UDP target IP:", UDP_IP)
-    f.write(", UDP target port:", UDP_PORT)
-    f.write(", message:", MESSAGE)
-    f.write(", @", time.clock())
-    f.write('\n')
-    sock = socket.socket(socket.AF_INET, # Internet
+    print "UDP target IP:", UDP_IP_SEND
+    print ", UDP target port:", UDP_PORT_SEND
+    print ", message:", MESSAGE
+    sendSock = socket.socket(socket.AF_INET, # Internet
                          socket.SOCK_DGRAM) # UDP
-    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+    sendSock.sendto(MESSAGE, (UDP_IP_SEND, UDP_PORT_SEND))
 
 def receive():
-    socket.setblocking(0)
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    f.write("received message:", data)
-    f.write(", @", time.clock())
-    f.write('\n')
+    try:
+        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+        "received message:", data
+    except:
+        stupidVar = 0
 
 if __name__ == '__main__':
     while True:
         send()
         receive()
-    # Thread(target = send).start()
-    # Thread(target = receive).start()
